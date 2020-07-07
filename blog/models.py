a0f08dcffbfd5fbcaaa3c804b8bd925ed825b432
DadaCat <venhal95@gmail.com>
2020-07-08 01:50:08 +0800

    添加后台编辑，添加第一天博文，存在bug需要修复

--- blog/models.py
+++ blog/models.py
@@ -1,35 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 #分类
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural= verbose_name
     def __str__(self):
         return self.name
 #标签
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
     def __str__(self):
         return self.name
 #文章
 class Post(models.Model):
     # 文章标题
-    title = models.CharField(max_length=70)
+    title = models.CharField('标题', max_length=70)
 
 
     # 文章正文，TextField可以存储更大文本
-    body = models.TextField()
+    body = models.TextField('正文')
 
     # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
 
     # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
-    excerpt = models.CharField(max_length=200, blank=True)
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name= '分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
